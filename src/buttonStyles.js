be8f37f261b9e4477bf715511a339d45c59f55ba
Adri Papavassiliu <papavassiliusol@gmail.com>
2022-02-07 18:33:58 +0100

    added generic button with basic styles

---
+++ src/buttonStyles.js
@@ -0,0 +1,7 @@
+import styled from "styled-components";
+
+export const ButtonGeneric = styled.button`
+  font-family: monospace;
+  border-radius: 10px;
+  text-align: center;
+`;
---
+++ src/components/Button/Button.js
@@ -0,0 +1,17 @@
+const ButtonComponent = ({ className, actionOnClick, text, type }) => {
+  return (
+    <>
+      {/* {type === "green" && (
+        <ButtonGreen className={className} onClick={actionOnClick}>
+          {text}
+        </ButtonGreen>
+      )}
+      {type === "red" && (
+        <ButtonRed className={className} onClick={actionOnClick}>
+          {text}
+        </ButtonRed>
+      )} */}
+    </>
+  );
+};
+export default ButtonComponent;
