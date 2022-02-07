3fb11fbd517cfb64cf0a2dff5c31f3230dc6cdf1
Adri Papavassiliu <papavassiliusol@gmail.com>
2022-02-07 20:38:46 +0100

    added styles to 4 types of buttons

--- src/jsStyles/buttonStyles.js
+++ src/jsStyles/buttonStyles.js
@@ -5,9 +5,44 @@ export const ButtonGeneric = styled.button`
   font-family: monospace;
   border-radius: 10px;
   text-align: center;
+  width: 120px;
+  height: 45px;
+  font-size: 16px;
+  font-family: monospace;
 `;
 
 export const ButtonWood = styled(ButtonGeneric)`
-  background-image: url("images/wooden-button.png") 300px 300px no-repeat;
-  color: red;
+  background-image: url("images/wooden-button.png");
+  background-position: center;
+  color: #fff;
+  object-fit: fill;
+  border-radius: none;
+  width: 250px;
+  height: 100px;
+  border-radius: none;
+`;
+
+export const ButtonOnCard = styled(ButtonGeneric)`
+  background-color: #2c514c;
+  border: 4px solid #ffffff;
+  box-sizing: border-box;
+  box-shadow: 4px 4px 4px rgba(0, 0, 0, 0.25);
+  border-radius: 20px;
+  color: #ffffff;
+`;
+
+export const ButtonFilter = styled(ButtonGeneric)`
+  background-color: rgba(196, 196, 196, 0.8);
+  border: 2px solid #2c514c;
+  border-radius: 17px;
+`;
+
+export const ButtonCreate = styled(ButtonGeneric)`
+  background-color: #fab827;
+  border: 4px solid #ffffff;
+  box-sizing: border-box;
+  box-shadow: 4px 4px 4px rgba(0, 0, 0, 0.25);
+  border-radius: 20px;
+  color: #ffffff;
+  font-size: 20px;
 `;
