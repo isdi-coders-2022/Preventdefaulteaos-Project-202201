34e4c54f60a6b139aa7bf3992ccebb33220f460a
Alejandro Rodríguez <a.rodriguezmachin@gmail.com>
2022-02-08 17:35:18 +0100

    added position to the background and button

--- src/components/PackOpenerContainer/PackOpenerContainer.js
+++ src/components/PackOpenerContainer/PackOpenerContainer.js
@@ -1,13 +1,30 @@
+import styled from "styled-components";
 import fakeArrayOfCards from "../../data/fakedata";
+import {
+  OpenPackButtonContainer,
+  OpenPackButtonContainerImage,
+} from "../../jsStyles/OpenPackButtonContainer";
 import Button from "../Button/Button";
 import PackOpenerCardList from "../PackOpenerCardList/PackOpenerCardList";
+
+const DivButton = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  position: absolute;
+`;
 
 const PackOpenerContainer = () => {
   const boosterPackAPI = fakeArrayOfCards;
 
   return (
     <>
-      <Button type="create" text="OPEN PACK!" />
+      <OpenPackButtonContainer>
+        <DivButton>
+          <Button type="create" text="OPEN PACK!" />
+        </DivButton>
+        <OpenPackButtonContainerImage src="images/OpenPackButtonContainer-background.png"></OpenPackButtonContainerImage>
+      </OpenPackButtonContainer>
       <PackOpenerCardList arrayOfCards={boosterPackAPI} />;
     </>
   );
---
+++ src/jsStyles/OpenPackButtonContainer.js
@@ -0,0 +1,19 @@
+import styled from "styled-components";
+
+export const OpenPackButtonContainer = styled.div`
+  display: flex;
+  position: relative;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  height: 150px;
+  width: 250px;
+`;
+
+export const OpenPackButtonContainerImage = styled.img`
+  z-index: -1;
+  height: 160px;
+  width: 260px;
+  position: absolute;
+  top: -30px;
+`;
