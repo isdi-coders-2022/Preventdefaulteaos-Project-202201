4bf7c0aa45a96d5c538cff2e92c4c497b57b4385
Alejandro Rodríguez <a.rodriguezmachin@gmail.com>
2022-02-08 11:23:15 +0100

    changed for to htmlFor in searchBar

--- src/components/PackOpenerCardList/PackOpenerCardList.js
+++ src/components/PackOpenerCardList/PackOpenerCardList.js
@@ -1,5 +1,4 @@
 import styled from "styled-components";
-import fakeArrayOfCards from "../../data/fakedata";
 import PackOpenerCard from "../PackOpenerCard/PackOpenerCard";
 
 const PackOpenerCardListContainer = styled.div`
