a94d3bfce35178634ed4c2040e04ee99606a3ede
Adri Papavassiliu <papavassiliusol@gmail.com>
2022-02-08 16:37:52 +0100

    created card component with propTypes

---
+++ src/components/Card/Card.js
@@ -0,0 +1,30 @@
+import Button from "../Button/Button";
+import PropTypes from "prop-types";
+
+const Card = ({ card: { imageUrl, name, types, colors, rarity } }) => {
+  return (
+    <div className="card">
+      <img src={imageUrl} alt={`${name} card`}></img>
+      <section>
+        <h3 className="card--name">{name}</h3>
+        <p className="card--type">Type: {types}</p>
+        <p className="card--color">Color: {colors}</p>
+        <p className="card--rarity">Rarity: {rarity}</p>
+        <Button type="onCard" text="MORE INFO" />
+        <Button type="onCard" text="ADD TO DECK" />
+      </section>
+    </div>
+  );
+};
+
+Card.propTypes = {
+  card: PropTypes.shape({
+    imageUrl: PropTypes.string,
+    name: PropTypes.string,
+    types: PropTypes.array,
+    colors: PropTypes.array,
+    rarity: PropTypes.string,
+  }),
+};
+
+export default Card;
