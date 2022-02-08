f6d230bdd57a47fcb271a8bd6b4d4cc9d4b4ed7f
Alejandro Rodríguez <a.rodriguezmachin@gmail.com>
2022-02-08 10:20:07 +0100

    added more data to fakedatacardarray

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-
 import PackOpenerCardList from "./components/PackOpenerCardList/PackOpenerCardList";
 
 import Button from "./components/Button/Button";
 import SearchBar from "./components/SearchBar/SearchBar";
 
-
 function App() {
   return (
     <>
-
-   
-      <PackOpenerCardList />
       <Button type="wood" text="uwu" />
       <SearchBar />
+      <PackOpenerCardList />
       {/* <Button type="onCard" text="More Info" />
       <Button type="filter" text="Kamigawa" />
       <Button type="wood" text="MAKE YOUR CARD" />
       <Button type="create" text="CREATE!" /> */}
-
     </>
   );
 }
