61f3acd4fdd98a19a73844d4ee65248d3ad52ad2
Alejandro Rodríguez <a.rodriguezmachin@gmail.com>
2022-02-08 16:01:56 +0100

    added a packOpenerContainer function to render the component packlist

---
+++ src/components/PackOpenerContainer/PackOpenerContainer.js
@@ -0,0 +1,7 @@
+import PackOpenerCardList from "../PackOpenerCardList/PackOpenerCardList";
+
+const PackOpenerContainer = () => {
+  return <PackOpenerCardList arrayOfCards={boosterPackAPI} />;
+};
+
+export default PackOpenerContainer;
