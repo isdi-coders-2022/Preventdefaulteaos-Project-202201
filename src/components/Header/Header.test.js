91b273c7bbba0da5b278a5cad111aad70791d303
Adri Papavassiliu <papavassiliusol@gmail.com>
2022-02-08 13:37:34 +0100

    tested header component

---
+++ src/components/Header/Header.test.js
@@ -0,0 +1,30 @@
+import { render, screen } from "@testing-library/react";
+import HeaderComponent from "./Header";
+
+describe("Given a Header component", () => {
+  describe("When it's instanciated", () => {
+    test("Then it should render the Header Component", () => {
+      render(<HeaderComponent />);
+
+      const headerRender = screen.getByRole("banner");
+
+      expect(headerRender).toBeInTheDocument();
+    });
+
+    test("Then it should render the image logo", () => {
+      render(<HeaderComponent />);
+
+      const headerRender = screen.getByRole("img");
+
+      expect(headerRender).toBeInTheDocument();
+    });
+
+    test("Then it should render the navigation bar", () => {
+      render(<HeaderComponent />);
+
+      const headerRender = screen.getByRole("navigation");
+
+      expect(headerRender).toBeInTheDocument();
+    });
+  });
+});
