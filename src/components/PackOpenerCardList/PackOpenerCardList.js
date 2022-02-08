64e5bd62ed74fc1975b6b298f2bbffa875b3c3ba
Alejandro Rodríguez <a.rodriguezmachin@gmail.com>
2022-02-08 11:40:59 +0100

    moved packopenerstyle to its folder

--- src/components/PackOpenerCardList/PackOpenerCardList.js
+++ src/components/PackOpenerCardList/PackOpenerCardList.js
@@ -1,15 +1,5 @@
-import styled from "styled-components";
 import PackOpenerCard from "../PackOpenerCard/PackOpenerCard";
-
-const PackOpenerCardListContainer = styled.div`
-  display: flex;
-  flex-direction: row;
-  flex-wrap: wrap;
-  justify-content: space-around;
-  width: 280px;
-  height: 400px;
-  gap: 10px;
-`;
+import PackOpenerCardListContainer from "../../jsStyles/packOpenerStyle";
 
 const PackOpenerCardList = ({ fakeArrayOfCards }) => {
   return (
