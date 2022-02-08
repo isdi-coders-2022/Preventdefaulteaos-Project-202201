865ef94c30ab346a0ebe96e82f1085eceeefd7b7
Alejandro Rodríguez <a.rodriguezmachin@gmail.com>
2022-02-08 08:46:05 +0100

    added styles to packopererCard with a div container

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import ButtonComponent from "./components/Button/Button";
+import PackOpenerCardList from "./components/PackOpenerCardList/PackOpenerCardList";
 
 function App() {
   return (
     <>
       <ButtonComponent type="wood" text="uwu" />
+      <PackOpenerCardList />
     </>
   );
 }
---
+++ src/components/PackOpenerCard/PackOpenerCard.js
@@ -0,0 +1,5 @@
+const PackOpenerCard = ({ card }) => {
+  return <img src={card.imageUrl} alt={card.name} />;
+};
+
+export default PackOpenerCard;
