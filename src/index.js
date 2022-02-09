c5dcfa6d5b4b8dba5526ed58aaa0c0d5d4b6709e
Alejandro Rodríguez <a.rodriguezmachin@gmail.com>
2022-02-09 09:26:21 +0100

    modified variables that we dont use

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from "react-dom";
 import App from "./App";
 import "./index.css";
 import { BrowserRouter } from "react-router-dom";
-import { Link, Navigate, Route, Routes } from "react-router-dom";
 
 import reportWebVitals from "./reportWebVitals";
 
