98dad886006fb6953b4b5935ae053f6f1469f429
Adri Papavassiliu <papavassiliusol@gmail.com>
2022-02-09 12:59:56 +0100

    loadBoosterCardsAction tested

---
+++ src/store/actions/BoosterCards/actionCreators.test.js
@@ -0,0 +1,21 @@
+import { loadBoosterCardsAction } from "./actionCreators";
+
+describe("Given a loadBoosterCardsAction function", () => {
+  describe("When it receives cards", () => {
+    test("Then it should return an action type load-BoosterCards", () => {
+      const boosterCards = [
+        { name: "Luis" },
+        { name: "Carlitos" },
+        { name: "Felix" },
+      ];
+      const expectedAction = {
+        type: "load-BoosterCards",
+        boosterCards,
+      };
+
+      const action = loadBoosterCardsAction(boosterCards);
+
+      expect(action).toEqual(expectedAction);
+    });
+  });
+});
