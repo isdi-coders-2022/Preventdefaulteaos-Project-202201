309b391ccf0720da3d41e98d62e9e2e048249554
Edu Ruiz <eya@MacBook-Air-de-Edu.local>
2022-02-09 10:50:31 +0100

    deleted unused vars

--- src/store/contexts/BoosterCardsContextProvider.js
+++ src/store/contexts/BoosterCardsContextProvider.js
@@ -1,9 +1,6 @@
-import { useReducer } from "react";
 import BoosterCardsContext from "./BoosterCardsContext";
 
 const BoosterCardsContextProvider = ({ children }) => {
-  const [boosterCards, dispatch] = useReducer();
-
   return (
     <BoosterCardsContext.Provider>{children}</BoosterCardsContext.Provider>
   );
