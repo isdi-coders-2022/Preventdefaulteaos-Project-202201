e2137505256142bb4e91d311e8875ce028eb044e
Edu Ruiz <eya@MacBook-Air-de-Edu.local>
2022-02-09 17:41:45 +0100

    deleted import fontawesome and faicon

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,5 +1,3 @@
-import { faBars } from "@fortawesome/free-solid-svg-icons";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { Header } from "../../jsStyles/HeaderStyles";
 import Button from "../Button/Button";
 
