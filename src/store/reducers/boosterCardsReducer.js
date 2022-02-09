b5edc0b6676a912eb98357dce7ed0875d61534fa
Edu Ruiz <eya@MacBook-Air-de-Edu.local>
2022-02-09 10:46:48 +0100

    created reducer for boosterCard

---
+++ src/store/reducers/boosterCardsReducer.js
@@ -0,0 +1,19 @@
+import actionsTypes from "../actions/BoosterCards/actionsTypes";
+
+const boosterCardsReducer = (currentBoosterCards, action) => {
+  let newBoosterCards;
+
+  switch (action.type) {
+    case actionsTypes.loadBoosterCards:
+      newBoosterCards = [...action.boosterCards];
+      break;
+    case actionsTypes.emptyBoosterCards:
+      newBoosterCards = [];
+      break;
+    default:
+      newBoosterCards = [...currentBoosterCards];
+  }
+  return newBoosterCards;
+};
+
+export default boosterCardsReducer;
