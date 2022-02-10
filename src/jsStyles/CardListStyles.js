ceee79b122299f6e255d7b50d747873399e5a9a4
Alejandro Rodríguez <a.rodriguezmachin@gmail.com>
2022-02-10 12:44:23 +0100

    changed styles to set the result cards in row

--- src/jsStyles/CardListStyles.js
+++ src/jsStyles/CardListStyles.js
@@ -1,17 +1,17 @@
 import styled from "styled-components";
 
-const CardListComponent = styled.div`
+export const H2 = styled.h2`
+  color: #fff;
+  margin: 0;
+  font-size: 25px;
+`;
+
+export const CardListComponent = styled.div`
   display: flex;
-  flex-direction: column;
+  flex-direction: row;
+  justify-content: center;
+  flex-wrap: wrap;
   gap: 18px;
   margin-top: 40px;
   align-items: center;
-
-  & h2 {
-    color: #fff;
-    margin: 0;
-    font-size: 25px;
-  }
 `;
-
-export default CardListComponent;
