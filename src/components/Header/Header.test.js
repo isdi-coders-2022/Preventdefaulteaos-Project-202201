9cab0ae269f3ac4f2561d593b216167722249e9b
Edu Ruiz <eya@MacBook-Air-de-Edu.local>
2022-02-10 13:17:15 +0100

    test fixed FloatingMenu and Header

--- src/components/Header/Header.test.js
+++ src/components/Header/Header.test.js
@@ -1,4 +1,5 @@
 import { render, screen } from "@testing-library/react";
+import { BrowserRouter } from "react-router-dom";
 import BoosterCardsContextProvider from "../../store/contexts/BoosterCardsContextProvider";
 import ResultsContextProvider from "../../store/contexts/ResultsContextProvider";
 import HeaderComponent from "./Header";
@@ -7,11 +8,13 @@ describe("Given a Header component", () => {
   describe("When it's instanciated", () => {
     test("Then it should render the Header Component", () => {
       render(
-        <BoosterCardsContextProvider>
-          <ResultsContextProvider>
-            <HeaderComponent />
-          </ResultsContextProvider>
-        </BoosterCardsContextProvider>
+        <BrowserRouter>
+          <BoosterCardsContextProvider>
+            <ResultsContextProvider>
+              <HeaderComponent />
+            </ResultsContextProvider>
+          </BoosterCardsContextProvider>
+        </BrowserRouter>
       );
 
       const headerRender = screen.getByRole("banner");
@@ -21,11 +24,13 @@ describe("Given a Header component", () => {
 
     test("Then it should render the image logo", () => {
       render(
-        <BoosterCardsContextProvider>
-          <ResultsContextProvider>
-            <HeaderComponent />
-          </ResultsContextProvider>
-        </BoosterCardsContextProvider>
+        <BrowserRouter>
+          <BoosterCardsContextProvider>
+            <ResultsContextProvider>
+              <HeaderComponent />
+            </ResultsContextProvider>
+          </BoosterCardsContextProvider>
+        </BrowserRouter>
       );
 
       const headerRender = screen.getByRole("img");
@@ -35,13 +40,15 @@ describe("Given a Header component", () => {
 
     test("Then it should render the navigation bar", () => {
       const text = "HOME";
-      
+
       render(
-        <BoosterCardsContextProvider>
-          <ResultsContextProvider>
-            <HeaderComponent />
-          </ResultsContextProvider>
-        </BoosterCardsContextProvider>
+        <BrowserRouter>
+          <BoosterCardsContextProvider>
+            <ResultsContextProvider>
+              <HeaderComponent />
+            </ResultsContextProvider>
+          </BoosterCardsContextProvider>
+        </BrowserRouter>
       );
 
       const headerRender = screen.getByText(text);
